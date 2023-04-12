ecda81237fd07bd529ac828d66fa49601ae27f12
Emanuel Rusu <erusu364@gmail.com>
2023-04-12 20:26:40 +0300

    added favorites heart to products

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleFavorite(icon) {
+    if (icon.classList.contains('bi-heart')) {
+        icon.classList.remove('bi-heart');
+        icon.classList.add('bi-heart-fill');
+        icon.style.color = 'red';
+    } else {
+        icon.classList.remove('bi-heart-fill');
+        icon.classList.add('bi-heart');
+        icon.style.color = '';
+    }
+}
