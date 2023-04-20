3aa397d51f920faf0fe84c736d0dd34741c0e824
Emanuel Rusu <erusu364@gmail.com>
2023-04-20 08:39:27 +0300

    adding to favorites works-heart/localstorage

--- index/script.js
+++ index/script.js
@@ -1,29 +1,64 @@
-function addToFavorites(cardId) {
+function updateHeartIcons() {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || {};
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+        const id = card.getAttribute('data-id');
+        const heartIcon = card.querySelector('.bi-heart');
+
+        if (favorites[id]) {
+            heartIcon.classList.add('red-heart');
+            heartIcon.classList.remove('bi-heart');
+            heartIcon.classList.add('bi-heart-fill');
+            heartIcon.style.color = 'red';
+        } else {
+            heartIcon.classList.remove('red-heart');
+            heartIcon.classList.remove('bi-heart-fill');
+            heartIcon.classList.add('bi-heart');
+            heartIcon.style.color = '';
+        }
+    });
+}
+
+window.addEventListener('load', () => {
+    updateHeartIcons();
+});
+
+function toggleFavorite(heartIcon) {
+    const card = heartIcon.closest('.card');
+    const id = card.getAttribute('data-id');
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || {};
+
+    if (favorites[id]) {
+        delete favorites[id];
+        heartIcon.classList.remove('red-heart');
+        removeFromFavorites(id, heartIcon);
+    } else {
+        favorites[id] = true;
+        heartIcon.classList.add('red-heart');
+        addToFavorites(id, heartIcon);
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    updateHeartIcons();
+}
+
+function addToFavorites(cardId, icon) {
     const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');
     favorites[cardId] = true;
     localStorage.setItem('favorites', JSON.stringify(favorites));
+
+    icon.classList.remove('bi-heart');
+    icon.classList.add('bi-heart-fill');
+    icon.style.color = 'red';
 }
 
-function removeFromFavorites(cardId) {
-    console.log('Removing item with ID:', cardId);
+function removeFromFavorites(cardId, icon) {
     const favorites = JSON.parse(localStorage.getItem('favorites') || '{}');
     delete favorites[cardId];
     localStorage.setItem('favorites', JSON.stringify(favorites));
-    console.log('Favorites after removal:', favorites);
-}
 
-function toggleFavorite(icon) {
-    const card = icon.closest('.card');
-    const cardId = card.dataset.id;
-    if (icon.classList.contains('bi-heart')) {
-        icon.classList.remove('bi-heart');
-        icon.classList.add('bi-heart-fill');
-        icon.style.color = 'red';
-        addToFavorites(cardId);
-    } else {
-        icon.classList.remove('bi-heart-fill');
-        icon.classList.add('bi-heart');
-        icon.style.color = '';
-        removeFromFavorites(cardId); // Call removeFromFavorites() here
-    }
+    icon.classList.remove('bi-heart-fill');
+    icon.classList.add('bi-heart');
+    icon.style.color = '';
 }
